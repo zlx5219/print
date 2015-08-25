c0b6cbb06999ab4714a7a5d4362f61efd41128c7
zhanglianxiang <zhanglianxiang@jd.com>
2015-08-25 19:36:49 +0800

    添加文件。

---
+++ src/main/java/print/PrintApplication.java
@@ -0,0 +1,12 @@
+package print;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrintApplication.class, args);
+    }
+}
---
+++ src/test/java/print/PrintApplicationTests.java
@@ -0,0 +1,18 @@
+package print;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = PrintApplication.class)
+@WebAppConfiguration
+public class PrintApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
